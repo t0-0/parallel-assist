d0ea4fab508aae9efc2d12faa4836b27285f668f
t0-0 <107523324+t0-0@users.noreply.github.com>
2024-07-12 23:05:22 +0900

    add tmp file

--- parallel-rsync.sh
+++ parallel-rsync.sh
@@ -4,8 +4,15 @@ remote_name=$1
 remote_dir=$2
 num_thread=$3
 local_dir=$4
+tmp_dir=$5
 
 # 転送するファイルをファイルサイズが大きい順に並べる
 file_list=$(ssh $1 "find $2 -type f | xargs ls -S --quoting-style=shell-always")
 
-mpirun -n $num_thread -x file_list="${file_list}" -x remote_name=${remote_name} -x remote_dir=${remote_dir} -x local_dir=${local_dir} bash rsync_inner.sh
+[ -d ${tmp_dir} ] && echo "${tmp_dir} must not exist" && exit 1
+mkdir -p ${tmp_dir}
+mkdir -p ${local_dir}
+
+mpirun -n $num_thread -x file_list="${file_list}" -x remote_name=${remote_name} -x remote_dir=${remote_dir} -x local_dir=${local_dir} -x tmp_dir=${tmp_dir} bash rsync_inner.sh
+
+rm -rf ${tmp_dir}
--- rsync_inner.sh
+++ rsync_inner.sh
@@ -6,30 +6,23 @@ for ((i=${OMPI_COMM_WORLD_RANK}; i<${#file_list[@]}; i+=$((2*${OMPI_COMM_WORLD_S
     file_name=${file_list[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
-    target_dir=$(dirname ${local_dir}/${file_name/${remote_dir}/''})
-    mkdir -p ${target_dir}
-    while true; do
-        rsync -avz ${remote_name}:${file_name} ${target_dir}
-        if [ $? -eq 0 ]; then
-            break
-        else
-            echo "try again"
-        fi
-    done
+    file_name=${file_name/${remote_dir}/''}
+    echo $file_name >>${tmp_dir}/${OMPI_COMM_WORLD_RANK}.txt
 done
 
 for ((i=$((2*${OMPI_COMM_WORLD_SIZE}-${OMPI_COMM_WORLD_RANK}-1)); i<${#file_list[@]}; i+=$((2*${OMPI_COMM_WORLD_SIZE})))) do
     file_name=${file_list[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
-    target_dir=$(dirname ${local_dir}/${file_name/${remote_dir}/''})
-    mkdir -p ${target_dir}
-    while true; do
-        rsync -avz ${remote_name}:${file_name} ${target_dir}
-        if [ $? -eq 0 ]; then
-            break
-        else
-            echo "try again"
-        fi
-    done
+    file_name=${file_name/${remote_dir}/''}
+    echo $file_name >>${tmp_dir}/${OMPI_COMM_WORLD_RANK}.txt
+done
+
+while true; do
+    rsync -avz --files-from="${tmp_dir}/${OMPI_COMM_WORLD_RANK}.txt" ${remote_name}:${remote_dir} ${local_dir}
+    if [ $? -eq 0 ]; then
+        break
+    else
+        echo "try again"
+    fi
 done
