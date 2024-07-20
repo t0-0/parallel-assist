3b257a30a1d333605defdcb0fb89c8fef4cd8465
t0-0 <107523324+t0-0@users.noreply.github.com>
2024-07-20 14:13:22 +0900

    add tar version check and env value check

--- parallel-thaw.sh
+++ parallel-thaw.sh
@@ -8,16 +8,26 @@ mkdir -p ${dest_dir}
 
 dest_dir=$(realpath ${dest_dir})
 
-tmp_list=($(tar tvf ${source_file} | sort -k5nr | awk '{print $9}'))
+tar_version=$(tar --version 2>&1)
+if echo ${tar_version} | grep -q "bsdtar"; then
+    tmp_str="$(tar tvf ${source_file} | sort -k5nr | awk '{print $9}')"
+elif echo ${tar_version} | grep -q "GNU tar"; then
+    tmp_str="$(tar tvf ${source_file} | sort -k3nr | awk '{print $6}')"
+else
+    echo "unknown tar"
+    exit 1
+fi
 
-file_list=()
+read -r -a tmp_array <<<$tmp_str
 
-for path in "${tmp_list[@]}"; do
+file_array=()
+
+for path in "${tmp_array[@]}"; do
     if [[ $path == */ ]]; then
         mkdir -p "${dest_dir}/${path}"
     else
-        file_list+=(${path})
+        file_array+=("${path}")
     fi
 done
 
-mpirun -n $num_thread -x file_list="${file_list[*]}" -x source_file=${source_file} -x dest_dir=${dest_dir} bash thaw-inner.sh
+mpirun -n $num_thread -x file_str="${file_array[*]}" -x source_file=${source_file} -x dest_dir=${dest_dir} bash thaw-inner.sh
--- thaw-inner.sh
+++ thaw-inner.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-cd ${dest_dir}
+if [ -z ${dest_dir} ]; then
+    exit 1
+elif [ -z ${source_file} ]; then
+    exit 1
+elif [ -z "${file_str}" ]; then
+    exit 1
+fi
 
-file_list=($file_list)
+cd "${dest_dir}" || exit 1
+
+read -r -a file_array <<<$file_str
 
 files_to_handle=()
 
-for ((i = ${OMPI_COMM_WORLD_RANK}; i < ${#file_list[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
-    file_name=${file_list[i]}
+for ((i = ${OMPI_COMM_WORLD_RANK}; i < ${#file_array[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
+    file_name=${file_array[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
-    files_to_handle+=(${file_name})
+    files_to_handle+=("${file_name}")
 done
 
-for ((i = $((2 * ${OMPI_COMM_WORLD_SIZE} - ${OMPI_COMM_WORLD_RANK} - 1)); i < ${#file_list[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
-    file_name=${file_list[i]}
+for ((i = $((2 * ${OMPI_COMM_WORLD_SIZE} - ${OMPI_COMM_WORLD_RANK} - 1)); i < ${#file_array[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
+    file_name=${file_array[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
-    files_to_handle+=(${file_name})
+    files_to_handle+=("${file_name}")
 done
 
 while [ ${#files_to_handle[@]} -ne 0 ]; do
