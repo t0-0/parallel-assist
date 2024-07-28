86909a10b9891de9a104b47385eee1debe6f4f92
t0-0 <107523324+t0-0@users.noreply.github.com>
2024-07-28 16:11:43 +0900

    validate arguments and rename

--- parallel-rsync.sh
+++ parallel-rsync.sh
@@ -8,11 +8,14 @@ tmp_dir=$5
 
 # 転送するファイルをファイルサイズが大きい順に並べる
 file_list=$(ssh $1 "find $2 -type f | xargs ls -S --quoting-style=shell-always")
+if [ $? -ne 0 ]; then
+    echo "must check remote_name or remote_dir"
+fi
 
 [ -d ${tmp_dir} ] && echo "${tmp_dir} must not exist" && exit 1
 mkdir -p ${tmp_dir}
 mkdir -p ${local_dir}
 
-mpirun -n $num_thread -x file_list="${file_list}" -x remote_name=${remote_name} -x remote_dir=${remote_dir} -x local_dir=${local_dir} -x tmp_dir=${tmp_dir} bash rsync_inner.sh
+mpirun -n $num_thread -x file_str="${file_list}" -x remote_name=${remote_name} -x remote_dir=${remote_dir} -x local_dir=${local_dir} -x tmp_dir=${tmp_dir} bash rsync_inner.sh
 
 rm -rf ${tmp_dir}
--- rsync_inner.sh
+++ rsync_inner.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-file_list=($file_list)
+if [ -z "${file_str}" ]; then
+    exit 1
+elif [ -z ${tmp_dir} ]; then
+    exit 1
+elif [ -z ${remote_name} ]; then
+    exit 1
+elif [ -z ${local_dir} ]; then
+    exit 1
+fi
 
-for ((i=${OMPI_COMM_WORLD_RANK}; i<${#file_list[@]}; i+=$((2*${OMPI_COMM_WORLD_SIZE})))) do
-    file_name=${file_list[i]}
+read -r -a file_array <<<$file_str
+
+for ((i = ${OMPI_COMM_WORLD_RANK}; i < ${#file_array[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
+    file_name=${file_array[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
     file_name=${file_name/${remote_dir}/''}
     echo $file_name >>${tmp_dir}/${OMPI_COMM_WORLD_RANK}.txt
 done
 
-for ((i=$((2*${OMPI_COMM_WORLD_SIZE}-${OMPI_COMM_WORLD_RANK}-1)); i<${#file_list[@]}; i+=$((2*${OMPI_COMM_WORLD_SIZE})))) do
-    file_name=${file_list[i]}
+for ((i = $((2 * ${OMPI_COMM_WORLD_SIZE} - ${OMPI_COMM_WORLD_RANK} - 1)); i < ${#file_array[@]}; i += $((2 * ${OMPI_COMM_WORLD_SIZE})))); do
+    file_name=${file_array[i]}
     file_name=${file_name#\'}
     file_name=${file_name%\'}
     file_name=${file_name/${remote_dir}/''}
